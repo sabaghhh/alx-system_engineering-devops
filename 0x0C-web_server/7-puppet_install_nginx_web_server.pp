2abef7ee01f64edacbeaf4e13e0532113a1547a9
sabaghhh <22-101257@students.eui.edu.eg>
2024-04-28 04:37:25 +0300

    done

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,18 +1,22 @@
-# Setup nginx server
+# Script to install nginx using puppet
+
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
+}
 
 package { 'nginx':
-  ensure     => 'installed',
+  ensure  => installed,
+  require => Exec['apt-get-update'],
 }
 
 file { '/var/www/html/index.html':
-  content => 'Holberton School',
+  content => 'Hello World!',
+  require => Package['nginx'],
 }
 
-file_line { 'aaaaa':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+exec {'redirect_me':
+  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell'
 }
 
 service { 'nginx':
